b07311a34e61243decb3bff7f6fcbf98ed408b6c
Nacho4321 <iarisnabarreta@fi.uba.ar>
2024-06-01 11:54:21 -0300

    Creación del Template

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+
+'''@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404'''
+
+
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="5000", debug=True)
