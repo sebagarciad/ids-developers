c93c8af424fa133c0247441777561c49774dad7e
sebagarciad <sebasgarcia@fi.uba.ar>
2024-06-02 18:00:06 -0300

    Se agrego en endpoint informacion-usuario (ID: 6657b5661c96671168da8d24)

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
-#Renderiza home
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/informacion-usuario")
+def info_usuario():
+    return render_template('informacion-usuario.html')
 
 
 '''@app.errorhandler(404)
