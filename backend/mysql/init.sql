f795740b5c52a9a712cd5f629855e8e337be672d
diegoocortez <dcortez@fi.uba.ar>
2024-06-13 15:53:10 -0300

    Modificación de tabla vuelos e inserts (ID: 6660a494ef2167f7fc8252c7)

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -8,16 +8,17 @@ CREATE TABLE IF NOT EXISTS aeropuertos (
 );
 
 CREATE TABLE IF NOT EXISTS vuelos (
-    id_vuelo INT AUTO_INCREMENT PRIMARY KEY,
-    codigo_aeropuerto_origen VARCHAR(3),
-    codigo_aeropuerto_destino VARCHAR(3),
-    hora_salida DATETIME,
-    hora_llegada DATETIME,
-    duracion TIME,
-    precio DECIMAL(10, 2),
-    pasajes_disponibles INT,
-    FOREIGN KEY (codigo_aeropuerto_origen) REFERENCES aeropuertos(codigo_aeropuerto),
-    FOREIGN KEY (codigo_aeropuerto_destino) REFERENCES aeropuertos(codigo_aeropuerto)
+id_vuelo INT AUTO_INCREMENT PRIMARY KEY,
+codigo_aeropuerto_origen VARCHAR(3),
+codigo_aeropuerto_destino VARCHAR(3),
+fecha_salida DATE,
+fecha_llegada DATE,
+hora_salida TIME,
+hora_llegada TIME,
+precio DECIMAL(10, 2),
+pasajes_disponibles INT,
+FOREIGN KEY (codigo_aeropuerto_origen) REFERENCES aeropuertos (codigo_aeropuerto),
+FOREIGN KEY (codigo_aeropuerto_destino) REFERENCES aeropuertos (codigo_aeropuerto)
 );
 
 
@@ -48,38 +49,29 @@ INSERT INTO aeropuertos VALUES ('NQN', 'Aeropuerto de Neuquen', 'Neuquen', 'Arge
 INSERT INTO aeropuertos VALUES ('USH', 'Aeropuerto de Ushuaia', 'Ushuaia', 'Argentina');
 INSERT INTO aeropuertos VALUES ('SLA', 'Aeropuerto de Salta', 'Salta', 'Argentina');
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','BCR','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',300000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','BCR','2024-07-20','2024-07-20','14:30:00','16:30:00',300000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','COR','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',250000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','COR','2024-07-20','2024-07-20','14:30:00','16:30:00',250000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','MDZ','2024-06-20 14:00:00','2024-06-20 16:30:00','02:30:00',350000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','MDZ','2024-07-20','2024-07-20','14:00:00','16:30:00',350000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','USH','2024-06-20 13:30:00','2024-06-20 16:30:00','03:00:00',400000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','USH','2024-07-20','2024-07-20','13:30:00','16:30:00',400000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('COR','BUE','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',300000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('COR','BUE','2024-07-20','2024-07-20','14:30:00','16:30:00',300000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','SLA','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',150000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','SLA','2024-07-20','2024-07-20','14:30:00','16:30:00',150000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('MDZ','BUE','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',300000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('MDZ','BUE','2024-07-20','2024-07-20','14:30:00','16:30:00',300000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('USH','BUE','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',300000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('USH','BUE','2024-07-20','2024-07-20','14:30:00','16:30:00',300000.00,150);
 
-INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, hora_salida, hora_llegada, duracion, precio, pasajes_disponibles)
-VALUES ('BUE','NQN','2024-06-20 14:30:00','2024-06-20 16:30:00','02:00:00',300000.00,150)
-;
+INSERT INTO vuelos (codigo_aeropuerto_origen, codigo_aeropuerto_destino, fecha_salida, fecha_llegada, hora_salida, hora_llegada, precio, pasajes_disponibles)
+VALUES ('BUE','NQN','2024-07-20','2024-07-20','14:30:00','16:30:00',300000.00,150);
